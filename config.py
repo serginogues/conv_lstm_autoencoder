4ae0f393b56431a90f50f2537c701a10df1a0e82
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2022-04-30 16:07:25 +0200

    train and test with any dataset

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BATCH_INPUT_SHAPE = 10
-SAVE_PATH = 'backup/model.hdf5'
+SAVE_PATH = 'backup/turnstiles.hdf5'
 BATCH_SIZE = 4  # number of training samples per learning iteration
 EPOCHS = 3  # number of times the full dataset is seen during training
 FORMATS_list = ["tif", "jpg", "png", "jpeg "]
