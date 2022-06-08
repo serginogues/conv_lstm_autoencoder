3b94efe1c43d4a137ecee090a26b6c2f69b1aff4
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2022-06-08 11:37:09 +0200

    ucsdped1 utils

--- dataset_utils.py
+++ dataset_utils.py
@@ -1,66 +1,56 @@
 from config import *
 from os import listdir
-import os.path
-from os.path import isfile, join, isdir
+from os.path import join, isdir, isfile
 import numpy as np
 from tqdm import tqdm
 import PIL.Image
 
 
-def get_clips_by_stride(frames_list: list):
+def create_UCSDPed1():
     """
     Parameters
     ----------
-    frames_list : list
-        A list of frames of shape 256 X 256. It should be a video sequence.
     Returns
     -------
-    list
-        A list of clips , 10 frames each (10 x 256 x 256 x 1)
+    np.array
+        numpy array of shape (# clips, BATCH_INPUT_LENGTH, IMAGE_SIZE, IMAGE_SIZE, 1)
     """
-    clips = []
-    clip = np.zeros(shape=(BATCH_INPUT_SHAPE, 256, 256, 1))
-    img_idx = 0
-    for img in frames_list:
-        clip[img_idx, :, :, 0] = img
-        img_idx = img_idx + 1
-        if img_idx == BATCH_INPUT_SHAPE:
-            clips.append(np.copy(clip))
-            img_idx = 0
-    return clips
-
-
-def get_train_dataset(dataset_path: str):
-    """
-    Parameters
-    ----------
-    dataset_path
-        path to dataset
-
-    Returns
-    -------
-    list
-        A list of clips , 10 frames each (10 x 256 x 256 x 1)
-    """
-    clips = []
+    dataset = []
+    ucsd_train_path = 'data/UCSDped1/Train'
     # loop over the training folders (video1, video2, ..)
-    for f in sorted(listdir(dataset_path)):
-        directory_path = join(dataset_path, f)
+    for f in sorted(listdir(ucsd_train_path)):
+        directory_path = join(ucsd_train_path, f)
         if isdir(directory_path):
+            # loop over all frames in the folder
             all_frames = []
-            # loop over all files in the folder
             for c in tqdm(sorted(listdir(directory_path)), desc="Loading " + str(f)):
                 img_path = join(directory_path, c)
-                # append if it is an image with FORMAT
-                if img_path.split(".")[-1] in FORMATS_list:
-                    img = PIL.Image.open(img_path).resize((256, 256))
+                # append if it is an image
+                if img_path.split(".")[-1] in IMAGE_EXTENSION_LIST:
+                    img = PIL.Image.open(img_path).resize((IMAGE_SIZE, IMAGE_SIZE))
                     img = np.array(img, dtype=np.float32) / 256.0
                     all_frames.append(img)
-            clips.extend(get_clips_by_stride(frames_list=all_frames))
-    return clips
+
+            # create clips and store in dataset
+            stride_list = [5, 10, 20] if DATA_AUGMENTATION else [TEMPORAL_STRIDE]
+            clips = []
+            for stride in stride_list:
+
+                """for start_frame_idx in [0, int(len(all_frames) * 0.25), int(len(all_frames) * 0.5),
+                                        int(len(all_frames) * 0.75)]:"""
+                clip = np.zeros(shape=(BATCH_INPUT_LENGTH, IMAGE_SIZE, IMAGE_SIZE, 1))
+                img_idx = 0
+                for i in range(0, len(all_frames), stride):
+                    clip[img_idx, :, :, 0] = all_frames[i]
+                    img_idx = img_idx + 1
+                    if img_idx == BATCH_INPUT_LENGTH:
+                        clips.append(np.copy(clip))
+                        img_idx = 0
+            dataset.extend(clips)
+    return np.asarray(dataset)
 
 
-def get_single_test(path: str):
+def get_single_test_UCSDPed1(path: str):
     """
     Parameters
     ----------
@@ -71,14 +61,12 @@ def get_single_test(path: str):
     np.ndarray
         (#images x 256 x 256 x 1) numpy array
     """
-    sz = int(len([name for name in os.listdir(path) if isfile(join(path, name)) and name.split(".")[-1] in FORMATS_list]))
-    test = np.zeros(shape=(sz, 256, 256, 1))
+    frame_path_list = [join(path, name) for name in listdir(path) if isfile(join(path, name)) and name.split(".")[-1] in IMAGE_EXTENSION_LIST]
+    test = np.zeros(shape=(int(len(frame_path_list)), 256, 256, 1))
     idx = 0
-    for f in tqdm(sorted(listdir(path)), desc="Get single test"):
-        file_path = join(path, f)
-        if file_path.split(".")[-1] in FORMATS_list:  # "tif"
-            img = PIL.Image.open(file_path).resize((256, 256))
-            img = np.array(img, dtype=np.float32) / 256.0
-            test[idx, :, :, 0] = img
-            idx = idx + 1
+    for img_path in frame_path_list:
+        img = PIL.Image.open(img_path).resize((256, 256))
+        img = np.array(img, dtype=np.float32) / 256.0
+        test[idx, :, :, 0] = img
+        idx = idx + 1
     return test
